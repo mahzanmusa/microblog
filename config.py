c9fee2e6707373aba10873afb5e7c4c7831042d2
Mahzan Musa <mahzan.musa@gmail.com>
2025-12-30 22:21:53 +0800

    Update config.py for email sending

--- config.py
+++ config.py
@@ -12,7 +12,15 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+ 
+    admins_env = os.getenv('ADMINS')
+    if admins_env:
+        # splits "a,b" into ['a', 'b'] and removes surrounding whitespace just in case
+        ADMINS = [email.strip() for email in admins_env.split(',')]
+    else:
+        # Fallback to empty list if not set
+        ADMINS = []
+
     POSTS_PER_PAGE = 25
 
     LANGUAGES = ['en', 'es']
